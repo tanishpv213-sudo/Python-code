6cacc9522029623711962f0b4cf563e99594aabf
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-07-07 18:23:40 +0200

    Add files via upload

---
+++ L4A1.py
@@ -0,0 +1,11 @@
+Variable1= 59
+Variable2= 87
+Variable3= 23
+Variable4= 54
+Variable5= 15
+
+Values = Variable1 + Variable2 + Variable3 + Variable4 + Variable5
+
+print("The sum of all five variables is:", Values)
+
+print("The average of all five variables is:", Values/5)
