6c0974aaf14a53c0ddc16598aae1591a8cb94d48
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-07-20 19:55:59 +0200

    Add files via upload

---
+++ ACP5.py
@@ -0,0 +1,7 @@
+
+temperature = float(input("Enter the current temperature (°C): "))
+
+if temperature >= 20:
+    print("You can wear light and soft clothes.")
+else:
+    print("Wear a jacket or pullover to stay warm.")
