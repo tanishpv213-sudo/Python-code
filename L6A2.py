da8c164bf8528e46e8752bd6607f1d7852d929b1
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-07-21 18:43:52 +0200

    Add files via upload

---
+++ L6A2.py
@@ -0,0 +1,16 @@
+a = 35
+b = 27
+c = 27
+print(not (a == b ))
+
+print(not (b == c))
+
+a = "summer"
+b = "winter"
+
+if  not (a == b):
+    print("a, 'summer', is different from b, 'winter'.")  
+a = int(input("Enter a number: "))
+if not (a % 2 == 0):
+    print(a, " is an odd number.")
+     
