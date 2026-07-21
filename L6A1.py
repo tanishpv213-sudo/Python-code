b1abeafb3fa7db0fb3e84f2a5901dad58c529281
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-07-21 18:28:01 +0200

    Add files via upload

---
+++ L6A1.py
@@ -0,0 +1,19 @@
+a = 20
+b = 38
+c = 0
+
+if a and b and c:
+    print("All the numbers are given boolean values of True.")
+else:
+    print("At least one of the numbers has a boolean value of False.")
+
+a = 20
+b = -87
+c = 0
+
+if a > 0 or b > 0 or c > 0:
+    print("Either all the numbers are positive or at least one of them is zero.")
+else:
+    print("All the numbers are negative.")
+
+    
