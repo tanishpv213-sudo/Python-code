6daa724d25e40feafa4b4096663015e63a9c9d26
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-07-27 17:11:08 +0200

    Add files via upload

---
+++ ACP7.py
@@ -0,0 +1,9 @@
+age = int(input("Enter student's age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Student is allowed to enroll in the class.")
+    else:
+        print("Student cannot enroll in the class.")
+else:
+    print("Student cannot enroll in the class.")
