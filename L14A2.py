9c9b55538721495ee2cd7b98f0818e8ea2a90f8b
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-08-18 16:25:52 +0200

    Add files via upload

---
+++ L14A2.py
@@ -0,0 +1,9 @@
+def cube(number):
+    return number*number*number
+def by_three(number):
+    if number % 3 == 0:
+        return cube(number)
+    else:
+        return False
+print(by_three(2))
+print(by_three(8))
