a6fa5177f2238b78e590e488b93400c7f903f640
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-09-06 18:38:14 +0200

    Add files via upload

---
+++ ACP17.py
@@ -0,0 +1,7 @@
+import math
+
+a = float(input("Enter angle: "))
+
+print("Sine =", math.sin(math.radians(a)))
+print("Cosine =", math.cos(math.radians(a)))
+print("Tangent =", math.tan(math.radians(a)))
