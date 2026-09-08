6facf5e18435f8f32c6dba68eabd0e85c06d47c7
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-09-08 16:30:31 +0200

    Add files via upload

---
+++ L19A1.py
@@ -0,0 +1,12 @@
+empty_list = []
+print(empty_list)
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triples = [x * 3 for x in [1, 2, 3]]
+print(triples)
+
+aList = [100 , 200 , 300 , 400 , 500]
+aList = aList[::-1]
+print(aList)
