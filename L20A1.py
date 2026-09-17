cb5d5c9567ea15b4c5b18c0462f8976f80516de1
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-09-17 20:52:34 +0200

    Add files via upload

---
+++ L20A1.py
@@ -0,0 +1,18 @@
+tuplex = ("tuple", True, 8.9, 4)
+print(tuplex)
+
+tuplex = (5, 9 , 7 , 11 , 8 , 3)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (60, 70 , 10 , 90 , 20)
+print(tuple1.count(50))
+
+tuplex = (2, 4 , 3 , 5 , 4 , 9 , 8 , 4 , 5)
+_slice = tuplex[3:5]
+print(_slice)
+
+_slice =  tuplex[ :6]
+print(_slice)
