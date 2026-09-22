7d489f027b7d8424a22c337ebd634da565de39cd
tanishpv213-sudo <tanishpv.213@gmail.com>
2026-09-22 16:35:06 +0200

    Add files via upload

---
+++ L22A2.py
@@ -0,0 +1,8 @@
+setx = ("green", "blue")
+sety = ("blue", "yellow")
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nThe intersection of the said sets:")
+setz = set(setx).intersection(sety)
+print(setz)
